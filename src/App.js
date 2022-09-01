5bcd61f53d02c75cdd90187fd58d6e84b1fd9164
mazanlabeeb <mazanlabeeb95@gmail.com>
2022-09-01 16:08:11 +0500

    lesson 44

--- src/App.js
+++ src/App.js
@@ -1,56 +1,74 @@
 import './App.css';
-import { Component } from 'react';
+import { Component, useState } from 'react';
 import React from 'react';
 import CardList from './components/card-list/card-list.component.jsx';
 import SearchBox from './components/search-box/search-box.component';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      users: [],
-      username: 'Mazan Labeeb',
-      searchQuery: ''
-    }
-  }
 
-  componentDidMount() { 
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(res => res.json())
-      .then(res => {
-        this.setState(() => {
-          return {
-            users: res
-          }
-        })
-      })
-  }
+const App = () => {
+  const [searchField, setSearchField] = useState("");
+  console.log(searchField);
 
-  onSearchChange = event => {
+  const onSearchHandler = event => {
     const searchQuery = event.target.value.trim();
-    this.setState(
-      () => {
-        return {
-          searchQuery
-        }
-      }
-    )
+    setSearchField(searchQuery);
   }
+  return (
+    <div className={"header"}>
+      <h1>Monsters Rolodex  </h1>
+      <input type="search" className={"className"} placeholder={"Search..."} onChange={onSearchHandler} />
 
-  render() {
-    const { users, searchQuery, username } = this.state;
-    const { onSearchChange } = this;
-
-    let filteredUsers = users.filter(val => val.name.toLowerCase().includes(searchQuery.toLowerCase()))
-    return (
-      <div className="mazan">
-        <SearchBox username={username} className = {"search-box"} onSearchHandler = {onSearchChange} placeholder = {"Search..."}/>
-        <CardList  filteredUsers={filteredUsers} />
-
-      </div>
-    );
-  }
+    </div>
+  );
 }
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       users: [],
+//       username: 'Mazan Labeeb',
+//       searchQuery: ''
+//     }
+//   }
+
+//   componentDidMount() { 
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then(res => res.json())
+//       .then(res => {
+//         this.setState(() => {
+//           return {
+//             users: res
+//           }
+//         })
+//       })
+//   }
+
+//   onSearchChange = event => {
+//     const searchQuery = event.target.value.trim();
+//     this.setState(
+//       () => {
+//         return {
+//           searchQuery
+//         }
+//       }
+//     )
+//   }
+
+//   render() {
+//     const { users, searchQuery, username } = this.state;
+//     const { onSearchChange } = this;
+
+//     let filteredUsers = users.filter(val => val.name.toLowerCase().includes(searchQuery.toLowerCase()))
+//     return (
+//       <div className="mazan">
+//         <SearchBox username={username} className = {"search-box"} onSearchHandler = {onSearchChange} placeholder = {"Search..."}/>
+//         <CardList  filteredUsers={filteredUsers} />
+
+//       </div>
+//     );
+//   }
+// }
 
 
 export default App;
